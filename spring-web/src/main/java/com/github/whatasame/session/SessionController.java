0ef0a81cfc6b1757745aa8f8725670afe3e9eada
whatasame <97666463+whatasame@users.noreply.github.com>
2023-12-21 00:49:17 +0900

    test: Add test for using default Spring session

---
+++ spring-web/src/main/java/com/github/whatasame/session/SessionController.java
@@ -0,0 +1,27 @@
+package com.github.whatasame.session;
+
+import jakarta.servlet.http.HttpSession;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/session")
+public class SessionController {
+
+    private static final String VISIT_COUNT = "visit-count";
+
+    @GetMapping({"/", ""})
+    public String session(final HttpSession session) {
+        if (session.isNew()) {
+            session.setAttribute(VISIT_COUNT, 1);
+
+            return "Greeting new user!";
+        } else {
+            final int count = (int) session.getAttribute(VISIT_COUNT);
+            session.setAttribute(VISIT_COUNT, count + 1);
+
+            return "Hello again, you have already visited this server " + count + " times.";
+        }
+    }
+}
---
+++ spring-web/src/test/java/com/github/whatasame/session/SessionTest.java
@@ -0,0 +1,56 @@
+package com.github.whatasame.session;
+
+import static org.hamcrest.Matchers.equalTo;
+
+import io.restassured.RestAssured;
+import io.restassured.filter.session.SessionFilter;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.server.LocalServerPort;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+class SessionTest {
+
+    @LocalServerPort
+    int port;
+
+    SessionFilter sessionFilter;
+
+    @BeforeEach
+    public void setup() {
+        RestAssured.baseURI = "http://localhost:" + port;
+        sessionFilter = new SessionFilter();
+    }
+
+    @Test
+    @DisplayName("서버에 처음 접속하면 새로운 세션이 생성된다.")
+    void newSession() {
+        RestAssured.given().when()
+                .get("/session")
+                .then()
+                .statusCode(200)
+                .body(equalTo("Greetings new user!"));
+    }
+
+    @Test
+    @DisplayName("서버에 두 번째 접속하면 기존 세션이 유지된다.")
+    void oldSession() {
+        RestAssured
+                .given()
+                .filter(sessionFilter)
+                .when()
+                .get("/session/")
+                .then()
+                .statusCode(200);
+
+        RestAssured.given().filter(sessionFilter)
+                .when()
+                .get("/session")
+                .then()
+                .statusCode(200)
+                .body(equalTo("Hello again, you have already visited this server 1 times."));
+    }
+}
